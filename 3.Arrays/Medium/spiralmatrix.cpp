6fb660c59feee125e99e82a0b74bf19dc0e62ea4
Aditya23042004 <riglunatic@gmail.com>
2024-09-22 22:14:46 +0530

    Spiral matrix added

---
+++ 3.Arrays/Medium/spiralmatrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>arr;
+        int top=0,bottom=matrix.size()-1,left=0,right=matrix[0].size()-1;
+        if(matrix.empty()){
+            return arr;
+        }
+        while(top <= bottom && left <= right){
+            for(int i = left ; i <= right ; i++){
+                arr.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i = top ; i <= bottom ; i++){
+                arr.push_back(matrix[i][right]);
+            }
+            right--;
+            if(top <= bottom){
+                for(int i = right ; i>= left ; i--){
+                    arr.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left <= right){
+                for(int i=bottom;i>=top;i--){
+                    arr.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return arr;
+        
+    }
+};
