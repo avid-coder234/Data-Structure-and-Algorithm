0fc861d37729f90b1889b8b28f4fae2d0449fa9a
Aditya23042004 <riglunatic@gmail.com>
2024-09-25 11:21:44 +0530

    files name changed and file added

---
+++ Binary Search/BSonAnswers/PainterPartition.cpp
@@ -0,0 +1,36 @@
+int countPainter(vector<int> &boards,int paint){
+    int time = 1    ;
+    int paintCount = 0;
+    int n = boards.size();
+
+    for(int i = 0 ; i < n ; i++){
+        if(paintCount + boards[i] <= paint){
+            paintCount += boards[i];
+        }else{
+            time += 1;
+            paintCount = boards[i];
+        }
+    }
+    return time;
+}
+
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    int low = *max_element(boards.begin(),boards.end());
+    int high = accumulate(boards.begin(),boards.end(),0);
+
+    while(low <= high){
+        int mid = (low + high) / 2;
+
+        int ans = countPainter(boards,mid);
+
+        if(ans > k){
+            low = mid + 1; 
+        }
+        else{
+            high = mid - 1;
+        }
+
+    }
+    return low;
+}
