e8c98cef452552bbddcec9a8fabf16b80fa73549
Aditya23042004 <riglunatic@gmail.com>
2024-09-28 00:43:04 +0530

    optimal soln 2

--- Arrays/Hard/mergetwosortedarray.cpp
+++ Arrays/Hard/mergetwosortedarray.cpp
@@ -22,3 +22,40 @@ void merge(long long arr1[],long long arr2[],int n,int m){
         sort(arr2,arr2 + m);
     }
 }
+
+//OPTIMAL SOLUTION 2 - Shell Sorting
+
+void swapGreater(long long arr1[],long long arr2[],int ind1,int ind2){
+    if(arr1[ind1] > arr2[ind2]){
+        swap(arr1[ind1],arr2[ind2]);
+    }
+}
+void merge(long long arr1[],long long arr2[],int n,int m){
+
+    int len = n + m;
+    int gap = (len / 2) + (len % 2);
+
+    while(gap > 0){
+        int left = 0;
+        int right = left + gap;
+
+        while(right < len){
+            // yaha pe left arr1 pe hai aur right arr2 pe 
+            if(left < n && right >= n){
+                swapGreater(arr1,arr2,left,right - n)
+            }
+            //yaha pe dono left aur right arr2 pe hai 
+            else if(left >= n){
+                swapGreater(arr2,arr2,left - n,right - n)
+            }
+            //yaha pe dono left aur right arr1 pe hai
+            else{
+                swapGreater(arr1,arr1,left,right)
+            }
+            left++;
+            right++;
+        }
+        if(gap == 1)break;
+        gap = (gap / 2) + (gap % 2);
+    }
+}
